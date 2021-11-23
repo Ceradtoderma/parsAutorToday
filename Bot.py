049113e18c936a7ff135b4aec5280ff51de5ccca
Cerastoderma <cerastoderma60@gmail.com>
2021-11-23 19:38:56 +0300

    Добавлены обработки ошибок

--- Bot.py
+++ Bot.py
@@ -14,10 +14,9 @@ logging.basicConfig(level=logging.INFO)
 data_dict = {}
 
 
-
 @dp.message_handler(commands="start")
 async def cmd_start(message: types.Message):
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True) #, one_time_keyboard=True)
     buttons = ["Логинимся", "Без Логина"]
     keyboard.add(*buttons)
     await message.answer('Парсер Autor today. Можем залогинится, можно попробовать спарсить без входа')
@@ -28,9 +27,9 @@ async def cmd_start(message: types.Message):
 async def log_off(message: types.Message):
     await message.answer('Введите ссылку')
 
+
 @dp.message_handler(Text(equals='Логинимся'))
 async def log_in(message: types.Message):
-    data_dict[message.from_user.id] = {}
     await message.answer('Введите логин и пароль через пробел')
 
 
@@ -40,7 +39,7 @@ async def parsing(message: types.Message):
     url = message.text
 
     await message.answer('Начинаем парсинг, подождите')
-    if data_dict:
+    if message.from_user.id in data_dict:
         data = data_dict[message.from_user.id]
         parser = ParsAT(url, data['login'], data['password'])
         threading.Thread(target=parser.login).start()
@@ -57,25 +56,35 @@ async def parsing(message: types.Message):
             cur_page = parser.cur_chapter
             await message.answer(str(cur_page))
 
-    await message.answer('Парсинг завершен')
+    if parser.state != 'ok':
+        await message.answer('Ошибка! текст ошибки: ')
+        if parser.error == 'Неверный логин или пароль.':
+            data_dict.clear()
+        await message.answer(f'"{parser.error}"')
+    else:
+        await message.answer('Парсинг завершен')
 
-
-    try:
-        await message.reply_document(open(parser.name + '.txt', 'rb'))
-    except:
-        await message.answer('Парсинг не удался')
+        try:
+            await message.reply_document(open(parser.name + '.txt', 'rb'))
+        except:
+            await message.answer('Парсинг не удался')
 
 
 @dp.message_handler(Text(contains='@'))
 async def parsing_login(message: types.Message):
-    data = data_dict[message.from_user.id]
-    data['login'] = message.text.split()[0]
-    data['password'] = message.text.split()[1]
-    print(data)
-    await message.answer('Введите ссылку')
+    if ' ' not in message.text:
+        await message.answer('Введите корректный логин пароль')
+    else:
+        data_dict[message.from_user.id] = {}
+        data = data_dict[message.from_user.id]
+        data['login'] = message.text.split()[0]
+        data['password'] = message.text.split()[1]
+        print(data)
+        await message.answer('Введите ссылку')
 
-
-
+@dp.message_handler()
+async def other(message: types.Message):
+    await message.answer('Парсер Autor today. Можем залогинится, можно попробовать спарсить без входа')
 
 
 if __name__ == "__main__":
--- parser.py
+++ parser.py
@@ -12,9 +12,10 @@ class ParsAT:
         self.name = name
         self.driver = webdriver.Chrome()
         self.text = ''
-
+        self.state = 'ok'
         self.pars = True
         self.cur_chapter = 'Начали'
+        self.error = 'Default'
 
     def login(self):
         self.driver.get('https://author.today/')
@@ -27,56 +28,64 @@ class ParsAT:
         self.driver.find_element(By.CLASS_NAME, 'modal-body').find_element(By.NAME, 'Password').send_keys(self.password)
         self.driver.find_element(By.CLASS_NAME, 'modal-body').find_element(By.CLASS_NAME, 'btn-primary').click()
         sleep(2)
-        self.get_text()
+
+        err = self.driver.find_element(By.CLASS_NAME, 'modal-body').find_element(By.CLASS_NAME, 'error-messages')
+
+        if err.text:
+            print(err.text)
+            self.state = 'err'
+            self.error = err.text
+            self.pars = False
+            self.driver.close()
+
+        else:
+            self.get_text()
 
     def get_text(self):
-        self.driver.get(self.url)
-        while True:
-            try:
-                sleep(2)
-                self.text += ' ' + self.driver.find_element(By.ID, 'text-container').text
-                btn = self.driver.find_element(By.CLASS_NAME, 'next').find_element(By.TAG_NAME, 'a')
-                print(btn.text)
-                self.cur_chapter = btn.text
-                btn.click()
-            except:
-                print('Страницы кончились')
-                break
-        self.save_text()
-        self.driver.close()
-        self.pars = False
 
+        if 'reader' not in self.url:
+            num = self.url.split('/')[-1]
+            self.url = 'https://author.today/reader/' + num
+
+        self.driver.get(self.url)
+
+        try:
+            text = self.driver.find_element(By.ID, 'text-container').text
+
+            while True:
+                try:
+                    sleep(2)
+                    self.text += ' ' + self.driver.find_element(By.ID, 'text-container').text
+                    btn = self.driver.find_element(By.CLASS_NAME, 'next').find_element(By.TAG_NAME, 'a')
+                    print(btn.text)
+                    self.cur_chapter = btn.text
+                    btn.click()
+                except:
+                    print('Страницы кончились')
+                    break
+
+            self.save_text()
+            self.driver.close()
+            self.pars = False
+        except:
+            err = self.driver.find_element(By.TAG_NAME, 'h1')
+
+            self.state = 'err'
+            if err.text:
+                self.error = err.text
+            else:
+                self.error = 'Неверная ссылка'
+            self.driver.close()
+            self.pars = False
+            print(self.error)
     def save_text(self):
         with open(f'{self.name}.txt', 'w', encoding='utf-8') as f:
             f.write(self.text)
 
 
 if __name__ == '__main__':
-    pars = ParsAT('https://author.today/reader/131901/1055496', '123', 'aisavikin@gmail.com', 'eto2016Detk*')
-    pars.login()
+    pass
+    # pars = ParsAT('https://author.today/reader/131901/1055496', 'aisavikin@gmail.com', 'eto2016Detk*', '123')
+    # pars.login()
     # pars.get_text()
-    print(pars.text)
-#
-# print(pars.text)
-
-
-# def get_text(url):
-#     text = ''
-#
-#     driver = webdriver.Chrome()
-#     driver.get(url)
-#     while True:
-#         try:
-#             text += ' ' + driver.find_element(By.ID, 'text-container').text
-#             btn = driver.find_element(By.CLASS_NAME, 'next').find_element(By.TAG_NAME, 'a')
-#             print(btn.text)
-#             btn.click()
-#             sleep(1)
-#         except:
-#             print('Страницы кончились')
-#             break
-#     return text
-#
-# def save_text(text, name):
-#     with open(f'{name}.txt', 'w') as f:
-#         f.write(text)
+    # print(pars.text)
